836baebc0aa4c536de2633047ec329886d0ceef3
Karel Rosseel <karel.rosseel@gmail.com>
2025-02-25 09:24:50 +0100

    bart

--- scripts/canvasLoadZim.js
+++ scripts/canvasLoadZim.js
@@ -1,79 +1,53 @@
-     
-var scaling = "fit"; 
+var scaling = "fit";
 var width = 1440;
 var height = 800;
-var color = "white";
+var color = "red";
 var outerColor = black;
-
 
 var path = "assets/";
 
-var assets = [
-    
-    "indirPNG.png",    
+var assets = [];
 
+var eklenecekResimlerListesi = ["boyama.png"];
 
-]
-
-
-var eklenecekResimlerListesi=["boyama.png","top.png","testResim.png","testResimInk.png"]
-
-for(var i=0; i<eklenecekResimlerListesi.length; i++){
-
-    assets.push(""+eklenecekResimlerListesi[i]+"")
+for (var i = 0; i < eklenecekResimlerListesi.length; i++) {
+  assets.push("" + eklenecekResimlerListesi[i] + "");
 }
 
-
-
-
-
 // these pages get loaded without preloading
-var later = [
+var later = [];
 
-];
+//var ilerleme = new ProgressBar({ barType: "rectangle" });
 
+//var stage;
+//var stageW;
+//var stageH;
 
-var ilerleme=new ProgressBar({barType:"rectangle"})
-
-var stage 
-var stageW 
-var stageH 
-
-
-var stage;
+//var stage;
 //var frame = new Frame(scaling, width, height, color, outerColor, assets, path,new Waiter());
-var frame=new Frame({
-    scaling:scaling,
-    width:width, 
-    height:height, 
-    color:color, 
-    outerColor:outerColor,
-    ready:ready, 
-    assets:assets, 
-    path:path,
-    progress:new Waiter()
+var frame = new Frame({
+  scaling: scaling,
+  width: width,
+  height: height,
+  color: color,
+  outerColor: outerColor,
+  ready: ready,
+  assets: assets,
+  path: path,
+  progress: new Waiter(),
 });
 
-
 function ready() {
-    zog("ready from ZIM Frame"); 
+  zog("ready from ZIM Frame");
 
-    stage = frame.stage;
-    stageW = frame.width;
-    stageH = frame.height;
+  stage = frame.stage;
+  //stageW = frame.width;
+  //stageH = frame.height;
 
-    createjs.Ticker.timingMode=createjs.Ticker.RAF;
-    createjs.Ticker.addEventListener("tick",stage); 
-    
-   
-    sekiller()
-    
+  //createjs.Ticker.timingMode = createjs.Ticker.RAF;
+  //createjs.Ticker.addEventListener("tick", stage);
 
-    
-    stage.update(); // this is needed to show any changes
-    
-    
+  sekiller();
 
-
+  stage.update(); // this is needed to show any changes
 } // end of ready
-    
